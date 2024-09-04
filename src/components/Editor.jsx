5052c0edb6e7aba7ffaf83bd6a76cd13886e0ed0
Pranjali Rathi <pranjalirathi05@gmail.com>
2024-09-04 11:02:21 +0530

    css

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,18 +1,43 @@
-import { Box } from '@mui/material';
+import { Box, styled } from '@mui/material';
+import CloseFullScreenIcon from '@mui/icons-material/CloseFullscreen';
+
+const Heading = styled(Box)`
+  background: #1d1e22;
+  display: flex;
+  padding: 9px 12px;
+
+`
+
+const Header = styled(Box)`
+  display: flex;
+  background: #060606;
+  color: #AAAEBC;
+  justify-content: space-between;
+  font-weight: 
+`
 
 const Editor = () => {
     return (
         <Box>
-            <Box>
-                <Box>
+            <Header>
+                <Heading>
                     <Box
                       component="span"
+                      style={{background: 'red',
+                              height: 20,
+                              width: 20,
+                              display: 'flex',
+                              placeContent: 'center',
+                              borderRadius: 5,
+                              marginRight: 5,
+                              paddingBottom: 2
+                      }}
                     >/
-
                     </Box>
                     HTML
-                </Box>
-            </Box>
+                </Heading>
+                <CloseFullScreenIcon/>
+            </Header>
             <Box>
 
             </Box>
