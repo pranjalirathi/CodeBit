b332ca75887f37d410b80e60509176123a59345d
Pranjali Rathi <pranjalirathi05@gmail.com>
2024-09-04 17:46:03 +0530

    result

--- src/components/ViewCode.jsx
+++ src/components/ViewCode.jsx
@@ -1,9 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { DataContext } from '../context/DataProvider';
 
 import { Box, styled } from '@mui/material';
 
+
+
 const ViewCode = () => {
+
+    const [source, setSource] = useState('');
+
+    const { html, css, js} = useContext(DataContext);
 
     const src = 
       `<html>
@@ -13,9 +19,23 @@ const ViewCode = () => {
       </html>
       `
 
+    useEffect(() => {
+        const timeout = setTimeout(() => { 
+            setSource(src)
+        }, 1000)
+
+        return () => clearTimeout(timeout);
+    }, [html, css, js])
+
     return (
         <Box>
-
+            <iframe 
+                srcDoc = {source}
+                title = "Output"
+                sandbox='allow-scripts'
+                width = "100%"
+                height = "100%"
+            />
         </Box>
     )
 }
